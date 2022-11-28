227f50d6b5a9526700706f0adfad4246989e644b
Matt Daneshvar <matt.daneshvar@gmail.com>
2022-11-27 21:04:41 -0600

    Limit pages to ones that have a parent

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -6,7 +6,7 @@ use Nortta\Laravel\Page;
 
 class QuestionsController extends Controller {
     public function index() {
-        return view('questions.index', ['questions' => Page::all()]);
+        return view('questions.index', ['questions' => Page::whereNotNull('parent_id')->get()]);
     }
 
     public function show(Page $page) {
